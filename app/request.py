ed517c76df52b141c319922a55ccd424ee1b6aaf
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-18 14:30:01 +0300

    Add request.py

---
+++ app/request.py
@@ -0,0 +1,13 @@
+# from app  import app
+# import urllib.request,json
+# from .models import quote
+# Quote = quote.Quote
+#
+# #Getting the api json
+# api_json = app.config['API_JSON']
+#
+# def get_quotes():
+#     """
+#     Function that gets json response
+#     """
+#     data =
