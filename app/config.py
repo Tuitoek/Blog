b124c91861ba6ab3a16db5840878f7c1aaf0896e
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-18 14:26:47 +0300

    add config.py

---
+++ app/config.py
@@ -0,0 +1,44 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    API_JSON="http://quotes.stormconsultancy.co.uk/random.json (Links to an external site.)"
+    SECRET_KEY='a77302eea2d2'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:bless32@localhost/tblog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
