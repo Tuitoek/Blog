0b66d4cd403f98920516b6f5d0d4baa9707b1fcf
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-18 14:30:14 +0300

    Add models.py

---
+++ app/models.py
@@ -0,0 +1,80 @@
+from . import db,login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(10))
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(100))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+
+class Quote():
+    '''
+    class to define quote objects
+    '''
+    def __init__(self,author,id,quote):
+        self.author=author
+        self.id = id
+        self.quote = quote
+        self.permalink = permalink
+
+class Blog(db.Model):
+    __tablename__ = 'blog'
+    """
+    Class to define blog objects
+    """
+    id = db.Column(db.Integer,primary_key = True)
+    author=db.Column(db.String(100))
+    title=db.Column(db.String(100))
+    description=db.Column(db.String(1000))
+    pic_path=db.Column(db.String(1000))
+    url=db.Column(db.String(10000))
+
+class Subscribe():
+    all_subscribe=[]
+    '''
+    class to define quote objects
+    '''
+    def __init__(self,author,id,quote):
+        self.email=email
+
+    # def save_subscribe(self):
+    #     Subscribe.all_subscribe.append(self)
+    #
+    #
+    # @classmethod
+    # def clear_reviews(cls):
+    #     Subscribe.all_subscribe.clear()
